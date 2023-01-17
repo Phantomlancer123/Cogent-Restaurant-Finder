7bf97f68851edd80719c078a7598c38c6a24b3c8
Phantomlancer123 <dracula1128bat@gmail.com>
2023-01-17 07:23:22 +0200

    make project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route, Navigate,
+} from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import {
+  HomePage,
+  DetailPage,
+} from './pages';
+
+import { lightTheme } from './styles';
+
+import { PATH } from './constants';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <Router>
+        <Routes>
+          <Route path={PATH.HOME} element={<HomePage />}> </Route>
+          <Route path={PATH.DETAIL} element={<DetailPage />}> </Route>
+          <Route path="*" element={<Navigate to={PATH.HOME} />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/constants/path.ts
@@ -0,0 +1,2 @@
+export const HOME = '/home';
+export const DETAIL = '/detail/:id';
